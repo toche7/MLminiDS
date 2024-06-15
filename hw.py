e1c6f37e8b36a0de1966cb3c9f27163ac5c53123
toche7 <toche7@hotmail.com>
2024-06-15 21:39:38 +0000

    add homework

---
+++ hw.py
@@ -0,0 +1,29 @@
+def homework():
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    df = pd.read_csv("https://raw.githubusercontent.com/toche7/DataSets/main/sale.txt", header = None)
+    df.columns = ['population','sale']
+    X = df[['population']]
+    y = df.sale
+
+    regEx1 = None
+    from sklearn.linear_model import LinearRegression
+    #regEx1 = LinearRegression()
+    #regEx1.fit(X,y)
+
+    #yhat = regEx1.predict(X)
+    
+    R2 = None
+    #R2 = regEx1.score(X,y)
+    
+    # plt.scatter(X,y)
+    # plt.plot(X,yhat,'r')
+    # plt.show()
+
+    return  regEx1, R2
+
+
+if __name__ == '__main__':
+    homework()
